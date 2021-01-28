c67b6a74e27468ad9f5b3590d508d551017a2802
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-28 12:38:39 +0000

    Updates config var name

--- src/features/setup/setupSlice.js
+++ src/features/setup/setupSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const API_URL = process.env.API_URL || 'http://localhost:8000/api/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/';
 
 export const setupSlice = createSlice({
   name: 'setup',
