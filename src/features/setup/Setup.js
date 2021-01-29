013463d0b42cf2ada8679e8e7dc6d7ec9e05ff5a
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-29 21:52:09 +0000

    Sends gameTypeIndex in create game

--- src/features/setup/Setup.js
+++ src/features/setup/Setup.js
@@ -6,16 +6,18 @@ import {
   getSetupResponse,
   getDeleteResponse,
 } from './setupSlice';
+import styles from './Setup.module.css';
 
 export function Setup() {
   const setupResponse = useSelector(getSetupResponse);
   const deleteResponse = useSelector(getDeleteResponse);
   const dispatch = useDispatch();
   const [playerNames, setPlayerNames] = useState('D,S');
+  const [gameTypeIndex, setGameTypeIndex] = useState(0);
 
   return (
     <div>
-        <button onClick={() => dispatch(createGame(playerNames))}>
+        <button onClick={() => dispatch(createGame(playerNames, false, gameTypeIndex))}>
             Init Game With Players
         </button>
         <br></br>
@@ -24,6 +26,11 @@ export function Setup() {
             value={playerNames}
             onChange={e => setPlayerNames(e.target.value)}
         />
+        <br></br>
+        <div onChange={e => setGameTypeIndex(e.target.value)}>
+            <label className={styles.displayBlock}>BlackJacks<input type='radio' value={0} name='gameTypeOptions' /></label>
+            <label className={styles.displayBlock}>Jacks, Twos and Eights<input type='radio' value={1} name='gameTypeOptions' /></label>
+        </div>
         <br></br>
         <div>{setupResponse}</div>
         <br></br>
